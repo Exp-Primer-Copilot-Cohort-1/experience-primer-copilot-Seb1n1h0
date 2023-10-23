0fad84b2af4d6cf479dab7bbfdf84438d3ffb654
Seb1n1h0 <64137204+Seb1n1h0@users.noreply.github.com>
2023-10-23 17:22:12 +0000

    Copilot second comit

---
+++ member.js
@@ -0,0 +1,69 @@
+function skillsMember() {
+    var member = {
+        name: 'John',
+        age: 30,
+        skills: ['js', 'html', 'css'],
+        salary: 2000
+    };
+    var member2 = {
+        name: 'John',
+        age: 30,
+        skills: ['js', 'html', 'css'],
+        salary: 2000
+    };
+    var member3 = {
+        name: 'John',
+        age: 30,
+        skills: ['js', 'html', 'css'],
+        salary: 2000
+    };
+    var member4 = {
+        name: 'John',
+        age: 30,
+        skills: ['js', 'html', 'css'],
+        salary: 2000
+    };
+    var member5 = {
+        name: 'John',
+        age: 30,
+        skills: ['js', 'html', 'css'],
+        salary: 2000
+    };
+    var member6 = {
+        name: 'John',
+        age: 30,
+        skills: ['js', 'html', 'css'],
+        salary: 2000
+    };
+    var member7 = {
+        name: 'John',
+        age: 30,
+        skills: ['js', 'html', 'css'],
+        salary: 2000
+    };
+    var member8 = {
+        name: 'John',
+        age: 30,
+        skills: ['js', 'html', 'css'],
+        salary: 2000
+    };
+    var member9 = {
+        name: 'John',
+        age: 30,
+        skills: ['js', 'html', 'css'],
+        salary: 2000
+    };
+
+    var members = [member, member2, member3, member4, member5, member6, member7, member8, member9];
+    console.log(members);
+    console.log(members[0]);
+    console.log(members[0].skills);
+    console.log(members[0].skills[0]);
+    console.log(members[0].skills[1]);
+    console.log(members[0].skills[2]);
+    console.log(members[0].skills[3]);
+    console.log(members[0].skills[4]);
+    console.log(members[0].skills[5]);
+    console.log(members[0].skills[6]);
+    console.log(members[0].skills[7]);
+}
